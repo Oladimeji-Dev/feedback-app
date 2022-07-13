6758d07e727417dfcf95df4c19159765b32edd2f
Oladimeji.Dev <salimanoladimeji33@gmail.com>
2022-07-13 14:55:22 +0100

    done with the about page commit

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@
 import React from "react";
 import { useState } from "react";
 import { v4 as uuidv4 } from "uuid";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./component/Header";
 import FeedbackData from "./FeedbackData";
 import FeedbackList from "./component/FeedbackList";
 import FeedbackStats from "./component/FeedbackStats";
 import FeedbackForm from "./component/FeedbackForm";
+import AboutPage from "./component/pages/AboutPage";
+import AboutIcon from "./component/AboutIcon";
 function App(){
     const [feedback, setFeedback] = useState(FeedbackData)
     const removeFeedback = (id) =>{
@@ -22,13 +25,23 @@ function App(){
 
     return(
         <div>
-            <Header />
-            <div className="container">
-                <FeedbackForm handleAddFeedback = {AddFeedback}/>
-                <FeedbackStats feedback={feedback} />
-                <FeedbackList feedback = {feedback} handleFeedback = {removeFeedback} />
-            </div>
-            
+            <Router>
+                <Header />
+                <div className="container">
+                    <Routes>
+                        <Route exact path="/" element = {
+                            <>
+                                <FeedbackForm handleAddFeedback = {AddFeedback}/>
+                                <FeedbackStats feedback={feedback} />
+                                <FeedbackList feedback = {feedback} handleFeedback = {removeFeedback} />
+                            </>  
+                        }>
+                        </Route>
+                        <Route path="/about" element = {<AboutPage />} />
+                    </Routes>
+                    <AboutIcon />
+                </div>
+            </Router>
         </div>
     )
 }
