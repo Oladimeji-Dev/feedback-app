f96b819bd14a38e36fd9c5727399f005f083a99f
Oladimeji.Dev <salimanoladimeji33@gmail.com>
2022-07-15 12:25:41 +0100

    created the edit feature

--- src/component/Feedback.jsx
+++ src/component/Feedback.jsx
@@ -1,16 +1,17 @@
-import {FaTimes} from "react-icons/fa"
+import {FaTimes, FaEdit} from "react-icons/fa"
 import Card from "./shared/Card"
 import {useContext} from 'react'
 import FeedbackContext from "../context/FeedbackContext"
 
 function Feedback({data}) {
-  const {removeFeedback} = useContext(FeedbackContext)
+  const {removeFeedback, editFeedback} = useContext(FeedbackContext)
   return (
     <Card reverse = {false}>
             <div className="num-display">{data.rating}</div>
             <button onClick={() =>removeFeedback(data.id)} className="close" >
               <FaTimes color="purple" />
             </button>
+            <button className="edit" onClick={()=>editFeedback(data)}><FaEdit color="purple"/></button>
             <div className="text-display">{data.text}</div>
     </Card>
    
--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,32 +1,63 @@
-import { createContext,useState } from "react";
+import { createContext, useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 
 const FeedbackContext = createContext()
 
 export const FeedbackProvider = ({children}) =>{
-    const [feedback,setFeedback] = useState([{
-        id: 1,
-        text:'this is from the context',
-        rating:10
-    }])
+    const [feedback,setFeedback] = useState([
+        {
+            id: 1,
+            text:'this is from the context 1',
+            rating:10
+        },
+        {
+            id: 2,
+            text:'this is from the context 2',
+            rating:9
+        },
+        {
+            id: 3,
+            text:'this is from the context 3',
+            rating:7
+        }
+    ])
 
+    const [feedbackEdit, setFeedbackEdit] = useState({
+        item:{},
+        edit:false
+    })
+
+    // Remove Feedback
     const removeFeedback = (id) =>{
         if(window.confirm("Are you sure you want to delete ?")){
             setFeedback(feedback.filter((item)=> item.id !==id))
         }
     }
 
+    //Add Feedback
     const AddFeedback = function(addedFeedback){
         addedFeedback.id = uuidv4()
         setFeedback([addedFeedback, ...feedback])
         
     }
 
+    //Edit Feedback
+    const editFeedback = function(item){
+        setFeedbackEdit(
+            {
+                item,
+                edit:true
+            }
+        )
+    }
+
+
     return<FeedbackContext.Provider
         value={{
             feedback,
             removeFeedback,
-            AddFeedback
+            AddFeedback,
+            editFeedback
         }}
     >
             {children}
